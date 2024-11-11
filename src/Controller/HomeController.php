6d99cc228f759ad6050ea63eec8868c42c666e3e
Adam Bassoumi <adam.bassoumi-EXT@continental-corporation.com>
2024-11-11 11:55:19 +0100

    adding produits to homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,12 +5,20 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ProduitRepository;
+
 
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'home')]
-    public function index(): Response
+    public function index(ProduitRepository $produitRepository): Response
     {
-        return $this->render('home/index.html.twig');
+        // Fetch all products
+        $produits = $produitRepository->findAll();
+
+        // Render the homepage with the products
+        return $this->render('home/index.html.twig', [
+            'produits' => $produits,
+        ]);
     }
 }
